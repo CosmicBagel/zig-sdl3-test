4d79840c692005480c9d5fcdac5ede68ec62a366
CosmicBagel <>
2025-07-05 23:45:24 -0600

    error wrapping (converting to zig error)

---
+++ src/SDLHelpers.zig
@@ -0,0 +1,24 @@
+/// Converts the return value of an SDL function to an error union.
+/// - Taken from: https://github.com/castholm/zig-examples/blob/master/opengl-hexagon/main.zig
+/// - Original name errify
+/// - SPDX-FileCopyrightText: NONE
+/// - SPDX-License-Identifier: CC0-1.0
+pub inline fn errorWrap(value: anytype) error{SdlError}!switch (@typeInfo(@TypeOf(value))) {
+    .bool => void,
+    .pointer, .optional => @TypeOf(value.?),
+    .int => |info| switch (info.signedness) {
+        .signed => @TypeOf(@max(0, value)),
+        .unsigned => @TypeOf(value),
+    },
+    else => @compileError("unerrifiable type: " ++ @typeName(@TypeOf(value))),
+} {
+    return switch (@typeInfo(@TypeOf(value))) {
+        .bool => if (!value) error.SdlError,
+        .pointer, .optional => value orelse error.SdlError,
+        .int => |info| switch (info.signedness) {
+            .signed => if (value >= 0) @max(0, value) else error.SdlError,
+            .unsigned => if (value != 0) value else error.SdlError,
+        },
+        else => comptime unreachable,
+    };
+}
--- src/main.zig
+++ src/main.zig
@@ -7,6 +7,9 @@ const c = @cImport({
 
 const std = @import("std");
 
+const SDLHelpers = @import("SDLHelpers.zig");
+const errorWrap = SDLHelpers.errorWrap;
+
 // SDL3 is handling the main func
 pub const main = c.main;
 
@@ -16,39 +19,68 @@ const background_rate = "waitevent";
 
 var window: ?*c.SDL_Window = null;
 var renderer: ?*c.SDL_Renderer = null;
+var gpu_device: ?*c.SDL_GPUDevice = null;
 
 pub export fn SDL_AppInit(appstate: ?*?*anyopaque, argc: c_int, argv: ?[*:null]?[*:0]u8) callconv(.c) c.SDL_AppResult {
     _ = appstate;
     _ = argc;
     _ = argv;
 
-    c.SDL_Log("zig sdl3 test");
+    c.SDL_Log("zig-sdl3-test");
 
-    _ = c.SDL_SetHint("SDL_MAIN_CALLBACK_RATE", foreground_rate);
-    _ = c.SDL_SetHint("SDL_RENDER_GPU_LOW_POWER", "1");
+    errorWrap(c.SDL_SetHint("SDL_MAIN_CALLBACK_RATE", foreground_rate)) catch {
+        c.SDL_Log("SDL_SetHint failed: %s", c.SDL_GetError());
+        return c.SDL_APP_FAILURE;
+    };
 
-    _ = c.SDL_SetAppMetadata("Example Renderer Clear", "1.0", "com.example.renderer-clear");
+    errorWrap(c.SDL_SetHint("SDL_RENDER_GPU_LOW_POWER", "1")) catch {
+        c.SDL_Log("SDL_SetHint failed: %s", c.SDL_GetError());
+        return c.SDL_APP_FAILURE;
+    };
 
-    if (!c.SDL_Init(c.SDL_INIT_VIDEO)) {
+    errorWrap(c.SDL_SetAppMetadata(
+        "zig-sdl3-test",
+        "0.0.1",
+        "com.cosmicbagel.zig-sdl3-test",
+    )) catch {
+        c.SDL_Log("SDL_SetAppMetadata failed: %s", c.SDL_GetError());
+        return c.SDL_APP_FAILURE;
+    };
+
+    errorWrap(c.SDL_Init(c.SDL_INIT_VIDEO)) catch {
         c.SDL_Log("Couldn't initialize SDL: %s", c.SDL_GetError());
         return c.SDL_APP_FAILURE;
-    }
+    };
 
-    if (!c.SDL_CreateWindowAndRenderer(
-        "examples/renderer/clear",
+    errorWrap(c.SDL_CreateWindowAndRenderer(
+        "zig-sdl3-test",
         640,
         480,
         c.SDL_WINDOW_RESIZABLE,
         &window,
         &renderer,
-    )) {
+    )) catch {
         c.SDL_Log("Couldn't create window/renderer: %s", c.SDL_GetError());
         return c.SDL_APP_FAILURE;
-    }
+    };
+
+    gpu_device = errorWrap(c.SDL_CreateGPUDevice(
+        c.SDL_GPU_SHADERFORMAT_MSL,
+        false,
+        null,
+    )) catch {
+        c.SDL_Log("SDL_CreateGPUDevice failed: %s", c.SDL_GetError());
+        return c.SDL_APP_FAILURE;
+    };
+
+    errorWrap(c.SDL_ClaimWindowForGPUDevice(gpu_device, window)) catch {
+        c.SDL_Log("SDL_ClaimWindowForGPUDevice failed: %s", c.SDL_GetError());
+        return c.SDL_APP_FAILURE;
+    };
 
     const error_check = c.SDL_GetError();
     if (c.strlen(error_check) > 0) {
-        c.SDL_Log("Misc init error %s", error_check);
+        c.SDL_Log("Misc SDL_AppInit error: %s", error_check);
         _ = c.SDL_ClearError();
     }
 
@@ -63,29 +95,34 @@ pub export fn SDL_AppIterate(appstate: ?*anyopaque) callconv(.c) c.SDL_AppResult
     const red: f32 = @floatCast(0.5 + 0.5 * c.SDL_sin(now));
     const green: f32 = @floatCast(0.5 + 0.5 * c.SDL_sin(now + c.SDL_PI_D * 2 / 3));
     const blue: f32 = @floatCast(0.5 + 0.5 * c.SDL_sin(now + c.SDL_PI_D * 4 / 3));
-    var did_error = c.SDL_SetRenderDrawColorFloat(renderer, red, green, blue, c.SDL_ALPHA_OPAQUE_FLOAT); // new color, full alpha.
-    if (!did_error) {
+
+    // new color, full alpha.
+    errorWrap(c.SDL_SetRenderDrawColorFloat(
+        renderer,
+        red,
+        green,
+        blue,
+        c.SDL_ALPHA_OPAQUE_FLOAT,
+    )) catch {
         c.SDL_Log("SetRenderDrawColorFloat %s", c.SDL_GetError());
         _ = c.SDL_ClearError();
-    }
+    };
 
     // clear the window to the draw color.
-    did_error = c.SDL_RenderClear(renderer);
-    if (!did_error) {
+    errorWrap(c.SDL_RenderClear(renderer)) catch {
         c.SDL_Log("RenderClear %s", c.SDL_GetError());
         _ = c.SDL_ClearError();
-    }
+    };
 
     // put the newly-cleared rendering on the screen.
-    did_error = c.SDL_RenderPresent(renderer);
-    if (!did_error) {
+    errorWrap(c.SDL_RenderPresent(renderer)) catch {
         c.SDL_Log("RenderPresent %s", c.SDL_GetError());
         _ = c.SDL_ClearError();
-    }
+    };
 
     const error_check = c.SDL_GetError();
     if (c.strlen(error_check) > 0) {
-        c.SDL_Log("Misc iterate error %s", error_check);
+        c.SDL_Log("Misc SDL_AppIterate error: %s", error_check);
         _ = c.SDL_ClearError();
     }
 
@@ -101,30 +138,28 @@ pub export fn SDL_AppEvent(appstate: ?*anyopaque, event: ?*c.SDL_Event) callconv
 
     if (event.?.type == c.SDL_EVENT_KEY_DOWN) {
         const scancode = event.?.key.scancode;
-        if (scancode == c.SDL_SCANCODE_ESCAPE or scancode == c.SDL_SCANCODE_Q) {
+        if (scancode == c.SDL_SCANCODE_ESCAPE) {
             return c.SDL_APP_SUCCESS;
         }
     }
 
     if (event.?.type == c.SDL_EVENT_WINDOW_FOCUS_GAINED) {
-        const did_error = c.SDL_SetHint("SDL_MAIN_CALLBACK_RATE", foreground_rate);
-        if (!did_error) {
+        errorWrap(c.SDL_SetHint("SDL_MAIN_CALLBACK_RATE", foreground_rate)) catch {
             c.SDL_Log("SetHint %s", c.SDL_GetError());
             _ = c.SDL_ClearError();
-        }
+        };
     }
 
     if (event.?.type == c.SDL_EVENT_WINDOW_FOCUS_LOST) {
-        const did_error = c.SDL_SetHint("SDL_MAIN_CALLBACK_RATE", background_rate);
-        if (!did_error) {
+        errorWrap(c.SDL_SetHint("SDL_MAIN_CALLBACK_RATE", background_rate)) catch {
             c.SDL_Log("SetHint %s", c.SDL_GetError());
             _ = c.SDL_ClearError();
-        }
+        };
     }
 
     const error_check = c.SDL_GetError();
     if (c.strlen(error_check) > 0) {
-        c.SDL_Log("Misc event error %s", error_check);
+        c.SDL_Log("Misc SDL_AppEvent error: %s", error_check);
         _ = c.SDL_ClearError();
     }
 
@@ -134,4 +169,9 @@ pub export fn SDL_AppEvent(appstate: ?*anyopaque, event: ?*c.SDL_Event) callconv
 pub export fn SDL_AppQuit(appstate: ?*anyopaque, result: c.SDL_AppResult) callconv(.c) void {
     _ = appstate;
     _ = result;
+
+    // is best to destroy things in reverse order of creation
+    c.SDL_DestroyGPUDevice(gpu_device);
+    c.SDL_DestroyRenderer(renderer);
+    c.SDL_DestroyWindow(window);
 }
