a69b538b306bfb6219f8b5f8178fb98f49ae6bc8
CosmicBagel <>
2026-09-22 18:16:32 -0600

    2d sprite vert & frag shader

--- src/frag.hlsl
+++ src/frag.hlsl
@@ -17,11 +17,16 @@ struct FragOutput
 FragOutput frag_shader(FragInput input) {
     FragOutput output;
 
-    const float pi = 3.141592653589793238462643383279502884;
-    output.color.r = 0.5 + 0.5 * sin(time + pi * input.color.r);
-    output.color.g = 0.5 + 0.5 * sin(time + pi * (2/3 + input.color.g));
-    output.color.b = 0.5 + 0.5 * sin(time + pi * (4/3 + input.color.b));
-    output.color.a = input.color.r * 1/3 + input.color.g * 1/3 + input.color.b * 1/3;
+    // const float pi = 3.141592653589793238462643383279502884;
+    // output.color.r = 0.5 + 0.5 * sin(time + pi * input.color.r);
+    // output.color.g = 0.5 + 0.5 * sin(time + pi * (2/3 + input.color.g));
+    // output.color.b = 0.5 + 0.5 * sin(time + pi * (4/3 + input.color.b));
+    // output.color.a = input.color.r * 1/3 + input.color.g * 1/3 + input.color.b * 1/3;
+
+    output.color.r = input.color.r;
+    output.color.g = input.color.g;
+    output.color.b = input.color.b;
+    output.color.a = input.color.a;
 
     return output;
 }
--- src/vert.hlsl
+++ src/vert.hlsl
@@ -1,9 +1,15 @@
 struct VertexInput
 {
     float3 position : POSITION;
-    float3 scale: SCALE;
-    float4 rotation: ROTATION;
     float4 color: COLOR;
+};
+
+[[vk::binding(0, 3)]]
+cbuffer VertexUniform : register(b0, space3)
+{
+    float2 world_position;
+    float2 scale;
+    float rotation;
 };
 
 struct VertexOutput
@@ -14,7 +20,18 @@ struct VertexOutput
 
 VertexOutput vert_shader(VertexInput input) {
     VertexOutput output;
-    output.position = float4(input.position, 1.0);
+
+    float c = cos(rotation);
+    float s = sin(rotation);
+    float2x2 rot_matrix = {c, s, 
+                           -s, c};
+    float2 rotated_pos = mul(rot_matrix, input.position.xy);
+
+    float2 scaled_pos = rotated_pos * scale;
+
+    output.position = float4(scaled_pos.xy, input.position.z, 1.0) + float4(world_position, 0.0, 0.0);
+
+    //output.position = float4(rotated_pos.xy, input.position.z, 1.0);
     output.color = input.color;
 
     return output;
